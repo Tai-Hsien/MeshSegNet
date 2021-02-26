8e091923249b5058abe917b07077ec36db1c4982
Tai-hsien <anigiss@gmail.com>
2021-02-26 16:17:18 -0500

    remove gco

--- gco-v3.0/matlab/GCO_Delete.m
+++
@@ -1,7 +0,0 @@
-function GCO_Delete(Handle)
-% GCO_Delete    Delete a GCoptimization object.
-%    GCO_Delete(Handle) deletes the object corresponding to Handle 
-%    and frees its memory.
-
-gco_matlab('gco_delete',int32(Handle));
-end
